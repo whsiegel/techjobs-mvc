48f4db84e6e5f3508196e4cbfe56726033e60e0a
William Siegel <whsiegel@gmail.com>
2019-06-06 10:22:08 -0500

    finish TODOS

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -4,6 +4,7 @@ import org.launchcode.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
@@ -22,6 +23,28 @@ public class SearchController {
         return "search";
     }
 
-    // TODO #1 - Create handler to process search request and display results
+    @RequestMapping(value = "results", method = RequestMethod.POST)
+    public String searchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
 
+        // use findByCandV for searchterms other than all
+        if (!searchType.equalsIgnoreCase("all")){
+            ArrayList<HashMap<String, String>> jobs = JobData.findByColumnAndValue(searchType,searchTerm);
+            model.addAttribute("columns", ListController.columnChoices);
+            model.addAttribute("title", "Jobs with " + searchType + ": " + searchTerm);
+            model.addAttribute("jobs", jobs);
+
+        }else{
+            ArrayList<HashMap<String,String>> jobs = JobData.findByValue(searchTerm);
+            model.addAttribute("columns", ListController.columnChoices);
+            model.addAttribute("title", "Jobs with " + searchType + ": " + searchTerm);
+            model.addAttribute("jobs", jobs);
+
+        }return "search";
+
+        /* This will be used for search by all
+
+        ArrayList<HashMap<String, String>> jobs = JobData.findByValue();
+
+         */
+    }
 }
